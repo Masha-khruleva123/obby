2693d6fc39c2b0971767a1e409ddc0c6b0a42c44
Masha-khruleva123 <113779155+Masha-khruleva123@users.noreply.github.com>
2022-09-17 16:12:26 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1663419746,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/банан-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/фон.png",
+		"images/блок.png",
+		"images/чертики.png",
+		"images/ужас.png",
+		"images/корона.png",
+		"images/tiledbackground3.png",
+		"images/sprite-sheet0.png",
+		"images/win-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
